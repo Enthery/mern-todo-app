44bbf6da3ee160ca37f8ef365a877e8391145e65
Pavel <enthery@mail.ru>
2024-11-11 18:33:56 +0300

    Update add-blog, header + styles

--- client/src/components/header/index.jsx
+++ client/src/components/header/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import classes from "./styles.module.css";
 
 export default function Header() {
@@ -5,8 +6,12 @@ export default function Header() {
     <div className={classes.header}>
       <h3>Mern Blog App</h3>
       <ul>
-        <li>Home</li>
-        <li>Add Blog</li>
+        <Link to={"/"}>
+          <li>Home</li>
+        </Link>
+        <Link to={"/add-blog"}>
+          <li>Add Blog</li>
+        </Link>
       </ul>
     </div>
   );
--- client/src/pages/add-blog/index.jsx
+++ client/src/pages/add-blog/index.jsx
@@ -1,7 +1,23 @@
+import classes from "./styles.module.css";
+
 export default function AddNewBlog() {
   return (
-    <div>
+    <div className={classes.wrapper}>
       <h1>Add A Blog</h1>
+      <div className={classes.formWrapper}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Enter Blog Title"
+          id="title"
+        />
+        <textarea
+          name="description"
+          placeholder="Enter Blog Description"
+          id="description"
+        />
+        <button>Add New Blog</button>
+      </div>
     </div>
   );
 }
